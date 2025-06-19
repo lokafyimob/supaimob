2e86eb6c75787174274d3a9271c5edfc7cea28bb
Lokafy <contato@lokafy.com.br>
2025-06-19 16:59:26 -0300

    fix: reset completo configuração Vercel - usando defaults Next.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@prisma/client'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client')
-    }
-    return config
-  },
-  env: {
-    SKIP_ENV_VALIDATION: '1'
-  },
   eslint: {
-    ignoreDuringBuilds: true,
-    dirs: [] // Don't lint any directories
+    ignoreDuringBuilds: true
   },
   typescript: {
     ignoreBuildErrors: true
